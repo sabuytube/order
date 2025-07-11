8caf8aec6761d4074b872bacac2e40cb5be7f3bc
sabuytube <sabuytube@gmail.com>
2025-07-11 09:30:03 +0700

    Add shop name row to summary table

--- src/app/summary/[id]/page.tsx
+++ src/app/summary/[id]/page.tsx
@@ -54,6 +54,11 @@ export default function SummaryPage() {
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td colSpan={3} className="p-2 font-semibold text-center">
+                {shopName}
+              </td>
+            </tr>
             {items.map((item, index) => (
               <tr key={index} className="border-b">
                 <td className="p-2">{item.name}</td>
