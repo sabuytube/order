670516ea1cb2c26d709007f1765f2022d8e36228
sabuytube <sabuytube@gmail.com>
2025-08-27 18:25:53 +0700

    feat: remove order column on summary page

--- src/app/summary/[id]/page.tsx
+++ src/app/summary/[id]/page.tsx
@@ -77,7 +77,6 @@ export default function SummaryPage() {
           <caption className="p-2 font-semibold text-center">{shopName}</caption>
           <thead>
             <tr className="border-b">
-              <th className="p-2 text-center">ลำดับ</th>
               <th className="p-2 text-left">ชื่อสินค้า</th>
               <th className="p-2">หน่วย</th>
               <th className="p-2">comment</th>
@@ -87,7 +86,7 @@ export default function SummaryPage() {
           <tbody>
             {items.map((item, index) => (
               <tr key={index} className="border-b">
-                <td className="p-2">{`${index + 1}. ${item.name}`}</td>
+                <td className="p-2">{item.name}</td>
                 <td className="p-2 text-center">{item.unit}</td>
                 <td className="p-2 text-center">{item.comment ?? ''}</td>
                 <td className="p-2 text-center">{item.unitPrice ?? ''}</td>
@@ -96,7 +95,7 @@ export default function SummaryPage() {
           </tbody>
           <tfoot>
             <tr>
-              <td colSpan={4} className="p-2 font-semibold text-right">รวม</td>
+              <td colSpan={3} className="p-2 font-semibold text-right">รวม</td>
               <td className="p-2 text-center font-semibold">{total}</td>
             </tr>
           </tfoot>
